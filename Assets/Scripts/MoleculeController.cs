fb39f2c2d342828a09182ae4ad431f930a6e5598
TheHighGroundWins <zolmonjuancarlos@gmail.com>
2022-10-12 12:03:36 -0400

    simulation actually works now

--- Assets/Scripts/MoleculeController.cs
+++ Assets/Scripts/MoleculeController.cs
@@ -10,6 +10,12 @@ public class MoleculeController : MonoBehaviour
     //repels
     private CircleCollider2D electronCollider;
 
+    [SerializeField]
+    private float energy = 5f;
+    
+    [SerializeField]
+    private float electronPush = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +25,12 @@ public class MoleculeController : MonoBehaviour
         for (i = 0; i < colliders.Length; i++)
         {
             //seperate the two colliders in order to differentiate collision
-            if (colliders[i].name == "Nucleus")
+            if (colliders[i].tag == "Nucleus")
             {
                 nucleusCollider = colliders[i];
             }
 
-            if (colliders[i].name == "Electron")
+            if (colliders[i].tag == "Electron")
             {
                 electronCollider = colliders[i];
             }
@@ -32,6 +38,12 @@ public class MoleculeController : MonoBehaviour
 
     }
     
+    //get and set functions
+    public float GetEnergy()
+    {
+        return energy;
+    }
+
     /*
      * for both of these
      * theres 3 different attractions
@@ -43,7 +55,7 @@ public class MoleculeController : MonoBehaviour
      * product of the two molecule energies over distance between them
      * (can be be attractive or repulsive force)
      *
-     * Van der Vals:
+     * Van der Waals:
      * 1/half the distance between two molecule
      * (always attraction of inverse distance)
      *
@@ -52,18 +64,51 @@ public class MoleculeController : MonoBehaviour
      * (always repulsive)
      */
 
-    private void OnTriggerEnter2D(Collider2D moleCollider2D)
+    private void OnTriggerEnter2D(Collider2D moleculeCollider2D)
     {
+        MoleculeBehavior(moleculeCollider2D);
     }
     
     void OnTriggerStay2D(Collider2D moleculeCollider)
     {
-        
+        MoleculeBehavior(moleculeCollider);
     }
+    
 
-    // Update is called once per frame
-    void Update()
+    void MoleculeBehavior(Collider2D moleculeCollider)
     {
+        //get difference between the two positions and normalize them
+        Vector2 direction = (transform.position - moleculeCollider.transform.position).normalized;
+
+        //variable to hold distance between two point
+        float distance = 0;
         
+        //coulombic and van der waals reaction
+        if (moleculeCollider.IsTouching(nucleusCollider) && 
+            !moleculeCollider.IsTouching(electronCollider) && moleculeCollider.tag=="Nucleus")
+        {
+            //calculate the absolute value distance between two points
+            distance = Vector2.Distance(moleculeCollider.transform.position, transform.position);
+
+            //coulombic
+            moleculeCollider.GetComponent<Rigidbody2D>().AddForce
+                (Temperature.temp*direction*((energy*
+              nucleusCollider.GetComponent<MoleculeController>().GetEnergy()) /distance));
+          
+          
+            
+            //van der waals
+            moleculeCollider.GetComponent<Rigidbody2D>().AddForce
+                (Temperature.temp*direction*(-1/(distance/2)));
+
+        }
+        
+        
+        //electron reaction
+        if (moleculeCollider.tag == "Electron" && moleculeCollider.IsTouching(electronCollider))
+        {
+            //repulsive/push force
+            moleculeCollider.GetComponentInParent<Rigidbody2D>().AddForce(Temperature.temp*direction*-electronPush); 
+        }
     }
 }
---
+++ Assets/Scripts/Temperature.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Temperature
+{
+    public static float temp = 10f;
+}
